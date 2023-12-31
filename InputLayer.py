b7d6baa1ae60f9a4eb8d3fa1400b3db7183e2b26
C046 <79424595+C046@users.noreply.github.com>
2023-12-31 12:25:56 -0800

    Update InputLayer.py

--- InputLayer.py
+++ InputLayer.py
@@ -6,35 +6,38 @@ Created on Sun Dec 31 11:10:07 2023
 """
 
 from neuralNetwork import *
+import random
 
 class InputLayer:
-    def __init__(self, num_neurons):
-        self.neurons = [Neuron(inputs=[random.uniform(-1, 1) for _ in range(num_neurons)],
-                               weights=[random.uniform(-1, 1) for _ in range(num_neurons)],
-                               bias=[random.uniform(-1, 1)])
+    def __init__(self, num_neurons, inputs, weights, bias):
+        self.neurons = [Neuron(inputs=inputs,
+                               weights=weights,
+                               bias=bias)
                         for _ in range(num_neurons)]
 
-        for neurons in self.neurons:
-            self.weights = neurons.weights
-            #print(self.weights)
-            self.inputs = neurons.inputs
+    def forward_propagate(self, activation="sigmoid", floats=True):
+        outputs = [neuron.forward_propagate(activationFunction=activation) for neuron in self.neurons]
+        if floats == True:
+            floats = []
+            for i in range(len(outputs)):
+                floats.append([float(II) for II in outputs[i]])
 
-            self.bias = neurons.bias
+            return floats
+        else:
+            del floats
 
-
-
-
-    def forward_propagate(self, inputs, activation="sigmoid"):
-            outputs = []
-            for neuron in self.neurons:
-                outputs.append(neuron.forward_propagate(activationFunction=activation))
-            for i in outputs:
-                print(i)
             return outputs
 
 
-inputlayer = InputLayer(5)
-sample_inputs = [0.5, 0.6, 0.7, 0.8, 0.9]
-outputs = inputlayer.forward_propagate(sample_inputs)
 
+
+
+
+num_neurons = 5
+sample_inputs = [0.5, 0.6, 0.7, 0.8, 0.9]
+weights = [random.uniform(-1, 1) for _ in range(num_neurons)]
+bias = [random.uniform(-1, 1) for _ in range(num_neurons)]
+
+inputlayer = InputLayer(num_neurons=num_neurons, inputs=sample_inputs, weights=weights, bias=bias)
+outputs = inputlayer.forward_propagate()
 print(inputlayer.neurons)
