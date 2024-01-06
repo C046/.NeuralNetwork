6f8709b0b4582826fb4ca370c1323ddcd4135829
C046 <79424595+C046@users.noreply.github.com>
2024-01-06 12:04:11 -0800

    Create NormalizationLoss.py

---
+++ NormalizationLoss.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  6 10:46:46 2024
+
+@author: hadaw
+"""
+
+import numpy as np
+
+class Normal:
+    def __init__(self, layer):
+        super().__init__()
+        self.layer = layer
+        self.size = layer.size
+
+    def _normalize_(self, layer, function):
+        return np.vectorize(function)(layer)
+
+    def absClassicalProbability(self, value):
+        return np.sum(np.linspace(0,abs(value)))/self.size
+
+    def classicalProbability(self, value):
+        num_outcomes = self.layer.size
+        return value/num_outcomes
+
+    def iterMatrix(self):
+        """
+        Iterate through the values of a matrix.
+
+        Parameters:
+            - matrix: numpy array or iterable
+            The matrix to iterate through.
+
+        Yields:
+            - int or float
+            Each individual value in the matrix.
+        """
+        if isinstance(self.layer, np.ndarray):
+        # If the input is a NumPy array, iterate through it directly
+            for value in self.layer.flatten():
+                yield value
+        else:
+            # If it's a generic iterable, iterate through rows and values
+            for row in self.layer:
+                for value in row:
+                    yield value
+
+
+matrix = np.array([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+
+normal = Normal(matrix)
+classicalProbDistribution = normal._normalize_(matrix, normal.classicalProbability)
+absClassicalProbability = normal._normalize_(classicalProbDistribution, normal.absClassicalProbability)
+# i can then use the klieber thing to predict the next value with these two distributions
